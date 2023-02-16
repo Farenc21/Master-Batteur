a10cbdcb8c0d7cdd6dda4d226305f8247e670544
Farenc Raphael <101197359+Farenc21@users.noreply.github.com>
2023-02-16 14:17:19 +0100

    changement -> agrandissement des images

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ window.addEventListener('load', function() {
     // Utilisation de GSAP pour animer la largeur de tous les éléments
     gsap.to(items, {
       width: item.clicked ? '15vw' : '8vw',
-      duration: 2,
-      ease: 'elastic(1, .6)'
+      duration: 1,
+      ease: 'power1.out'
     });
     
     // Je Modifie la valeur de la propriété clicked de l'élément cliqué
     item.clicked = !item.clicked;
     gsap.to(item, {
       width: item.clicked ? '42vw' : '15vw',
-      duration: 2.5,
-      ease: 'elastic(1, .3)'
+      duration: 1,
+      ease: 'power1.out'
     });
 
     // Pour chaque élément .text, vérifie s'il correspond à l'élément .item cliqué
